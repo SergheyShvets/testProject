4e5246977dec647b1cb6b5802e4d73caf9ca8646
svet <svet-serghey@yandex.ru>
2022-09-03 19:18:08 +0300

    added error

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         int number = 20;
         String name = "Сергей";
         System.out.println(name + ", Ваша зарплата равна " + number);
-    }
+    }*/
 
     ///Ilia dobavi eshio familiu
 }

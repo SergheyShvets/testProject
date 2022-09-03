e81cff3fe3b9722869ff3101da8bda59bc5d2b01
Ilia <ilia19011991@gmail.com>
2022-09-03 20:42:31 +0300

    add profession

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int number = 20;
         String fio = "Власов Сергей Сергеевич";
-        System.out.println(fio + ", Ваша зарплата равна " + number); вяиваича
+        System.out.println(fio + ", Ваша зарплата равна " + number);
+        String profession = "developer";
+        System.out.println( "Ваша профессия " + profession);
     }
 
 }

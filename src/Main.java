e56c94c9dfc3d11c7d2467ab11470accb21ac801
Ilia <ilia19011991@gmail.com>
2022-09-03 19:06:08 +0300

    Add name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int number = 20;
-        System.out.println("Ваша зарплата равна " + number);
+        String name = "Сергей";
+        System.out.println(name + ", Ваша зарплата равна " + number);
     }
 }

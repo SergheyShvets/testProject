30e81cff54b8e03ab1417ed37579a453a5631033
svet <svet-serghey@yandex.ru>
2022-09-03 18:38:53 +0300

    changed description

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int number = 20;
-        System.out.println(number);
+        System.out.println("Ваша зарплата равна " + number);
     }
 }
